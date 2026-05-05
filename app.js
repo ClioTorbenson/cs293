217ff98a778e78b24a18251972e8dc3fa0cabb3b
ClioTorbenson <lc22-0873@lclark.edu>
2026-05-05 16:43:19 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,4 @@
-// console.log('Start');
 
-// setTimeout(()=>{
-//     console.log('Inside Timeout');
-// }, 2000);
-
-// console.log('End'); 
-
-// const http = require("http");
-// const fs = require("fs");
-
-// const server = http.createServer((req, res) => {
-//     fs.readFile("index.html", (err, data) => {
-//         res.writeHead(200, {"Content-Type": "text/html"})  // status code, then type of data to write
-//         res.write(data);  // reading the file index.html and writing it in the server
-//         res.end();  // stops waiting for response
-//     })
-// });
-
-// server.listen(3000, ()=> {
-//     console.log('Server running on http://localhost:3000');
-// })
-
-// const express = require("express");
-// const app = express();
-
-// app.use(express.urlencoded({extended:false}));
-
-// app.post("/form", (req, res)=> {
-//     res.send(req.body.name);
-// })
-
-// app.get("/user/:name", (req, res) => {
-//     res.send(`Hello, ${req.params.name}`);  // makes url dynamic
-// })
-
-// app.listen(3000, () => {
-//     console.log('Server running on http://localhost:3000')
-// });
 
 const express = require("express");
 const path = require("path");
@@ -45,6 +7,42 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 
+const mysql = require('mysql2')
+const connection = mysql.createConnection
+({
+    host: 'localhost',
+    user: 'root',
+    password: 'cliotorbenson', //your password that you set while installing the database
+    database: 'formSubmission' //your database name
+
+})
+connection.connect((err)=>{
+
+  if(err) {
+    console.error('Connection error:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL!')
+});
+
+app.get('/', (req,res)=> {
+res.sendFile(path.join(__dirname, "public/contact.html"));})
+
+
+app.post("/submit", (req,res) => {
+    console.log("Received data:", req.body);
+  const { firstName, lastName, email, message } = req.body;
+  const sql = 'INSERT INTO contactInfo (firstName, lastName, email, message) VALUES (?, ?, ?, ?)'
+  connection.query(sql, [firstName, lastName, email, message], (err, result) => {
+        if (err) {
+            console.error('Insert Error:', err.message);
+            return res.status(500).send("Database error occurred.");
+        }
+        
+        console.log('Data inserted, ID:', result.insertId);
+        res.redirect("/");
+    });
+})
 
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
